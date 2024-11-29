a8a0aef16003c5c291aa8c9bfdd7cfac59da301d
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-11-29 20:25:50 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        (search button)
+        articles/blog?
+        about
+        projects
+      </div>
+    </>
+  )
+}
+
+export default App
